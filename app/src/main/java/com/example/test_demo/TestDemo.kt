20b066b0b853adb20b38922287094ddcec07c550
Hotchpotch <machismo@qq.com>
2021-09-24 00:56:23 +0800

    多用户提交测试

---
+++ app/src/main/java/com/example/test_demo/TestDemo.kt
@@ -0,0 +1,17 @@
+package com.example.test_demo
+
+fun main() {
+    val intNumber: Int = 100
+    val longNumber: Long = 8000000000
+    val byteNumber: Byte = 1
+    val floatNumber: Float = 3.1f
+    val doubleNumber: Double = 3.141592654
+    val shoutNumber: Short = 12
+    println("intNumber:$intNumber")
+    println("longNumber:$longNumber")
+    println("byteNumber:$byteNumber")
+    println("floatNumber:$floatNumber")
+    println("doubleNumber:$doubleNumber")
+    println("shoutNumber:$shoutNumber")
+    println("shoutNumber:$shoutNumber")
+}
